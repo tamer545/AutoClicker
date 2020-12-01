3d2ab411b31b91a47c939a29815bf96e840fb7dc
timoa <timo-angst@bluewin.ch>
2020-12-01 10:19:43 +0100

    le autoclicker

--- src/AutoClicker/AutoClickerView.java
+++ src/AutoClicker/AutoClickerView.java
@@ -1,28 +1,34 @@
 package AutoClicker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class AutoClickerView extends JFrame {
+    private KeyboardFocusManager keyboardFocusManager;
     private AutoClickerPresenter presenter;
     private JTextField delayField;
     private JButton confirmButton;
     private JPanel mainPanel;
+    private JButton STOPButton;
 
 
     public AutoClickerView() {
         super("AutoClicker");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800, 450);
+        setSize(550, 450);
         setContentPane(mainPanel);
+        mainPanel.isFocusable();
         setVisible(true);
+
+
         confirmButton.addActionListener(e -> {
             presenter.confirm(Integer.parseInt(delayField.getText()));
 
         });
-        confirmButton.addKeyListener(new KeyListener() {
+        STOPButton.addActionListener(e -> System.exit(0));
 
-
+        KeyListener keyListener = new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
 
@@ -31,7 +37,13 @@ public class AutoClickerView extends JFrame {
             @Override
             public void keyPressed(KeyEvent e) {
                 while (e.getKeyCode() == KeyEvent.VK_P) {
-                    presenter.execute();
+                    if(!mainPanel.hasFocus()) {
+                        presenter.execute();
+                    }
+
+                }
+                if (e.getKeyCode() == KeyEvent.VK_O) {
+                    System.exit(0);
 
                 }
             }
@@ -41,7 +53,11 @@ public class AutoClickerView extends JFrame {
 
             }
 
-        });
+        };
+        confirmButton.addKeyListener(keyListener);
+        mainPanel.addKeyListener(keyListener);
+
+
 
     }
 
